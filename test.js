83a32f94085c8332c1906014a5280cdb9d39774c
shoeb-Akther <shoebakther13@gmail.com>
2025-03-02 06:01:16 +0600

    first commit

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ function formatAMPM(date) {
     var seconds = date.getSeconds();
     var ampm = hours >= 12 ? 'pm' : 'am';
     hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
+    hours = hours ? hours : 12; 
     minutes = minutes < 10 ? '0'+minutes : minutes;
     var strTime = hours + ':' + minutes + ':' +seconds + ' ' + ampm;
     return strTime;
